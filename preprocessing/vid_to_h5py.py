25ba153d44869c1a1f797c7db73cc57836db939a
Rahul Singh <rahulsinghk998@gmail.com>
2016-10-23 21:41:01 +0530

    Change auto completion and deletion process

--- preprocessing/vid_to_h5py.py
+++ preprocessing/vid_to_h5py.py
@@ -4,6 +4,10 @@ from __future__ import print_function
 # import imageio
 import os
 import h5py
+import cv2
+import numpy as np
+import sys
+import skvideo.io
 from glob import glob
 
 import subprocess
@@ -12,8 +16,8 @@ from scipy.misc import imread, imresize
 """
 Taken help from: https://github.com/EderSantana/seya/tree/master/seya/preprocessing
 """
-def vid_to_hdf5(hdf5file, filepath,
-                           ext='*.avi',
+def vid_to_hdf5(hdf5file='test.h5', filepath='/home/rahulsingh/research/preprocessing/',
+                           ext='*.mp4',
                            img_shape=(224, 224),
                            frame_range=range(0, 20),
                            row_range=slice(120-112, 120+112),
@@ -40,34 +44,75 @@ def vid_to_hdf5(hdf5file, filepath,
     rlen = row_range.stop - row_range.start
     clen = col_range.stop - col_range.start
     files = glob(os.path.join(filepath, ext))
+    f='/home/rahulsingh/research/preprocessing/small.mp4'
+    c1=0
     with h5py.File(hdf5file, 'w') as h5:
-        # create datasets
-        X = h5.create_dataset('data', (len(files), len(frame_range),
-                              rlen, clen, 3), dtype='f')
-        if label_callback is not None:
-                label_size = label_callback('test', return_len=True)
-                y = h5.create_dataset('labels', (len(files), label_size), dtype='f')
+        process = subprocess.Popen('mkdir {}'.format(f[:-4]), shell=True, stdout=subprocess.PIPE)
+        process.wait()
+        cmd = converter + " -i {0} -r 1/1 {1}/%03d.jpg".format(f, f[:-4])
+        process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+        process.wait()
 
-        for c1, f in enumerate(files):
-            process = subprocess.Popen('mkdir {}'.format(f[:-4]), shell=True, stdout=subprocess.PIPE)
-            process.wait()
-            cmd = converter + " -i {0} -r 1/1 {1}/%03d.jpg".format(f, f[:-4])
-            process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-            process.wait()
+        imgs = glob(os.path.join(f[:-4], '*.jpg'))
+        X = h5.create_dataset('data', (len(imgs), rlen, clen, 3), dtype='f')
+        for c2, im in enumerate(imgs[:]):#frame_range[-1]]):
+            #print(frame_range[-1])
+            I = imread(im)[row_range, col_range]
+            if I.shape[:2] != img_shape:
+                I = imresize(imread(im), img_shape)
+            print(c2)
+            X[c2] = I
 
-            imgs = glob(os.path.join(f[:-4], '*.jpg'))
-            for c2, im in enumerate(imgs[:frame_range[-1]]):
-                I = imread(im)[row_range, col_range]
-                if I.shape[:2] != img_shape:
-                    I = imresize(imread(im), img_shape)
-                X[c1, c2] = I
+        process = subprocess.Popen('rm -r {}'.format(f[:-4]), shell=True, stdout=subprocess.PIPE)
+        process.wait()
 
-            if label_callback is not None:
-                y[c1] = label_callback(f)
-
-            process = subprocess.Popen('rm {}'.format(f[:-4]), shell=True, stdout=subprocess.PIPE)
-            process.wait()
 
     with open(hdf5file+'.txt', 'w') as file_order:
         for f in files:
             file_order.write(f+'\n')
+
+
+
+"""
+    Apparently opencv 'Videowriter' has a bit of codec issue problems which 
+    according to some websites gets solved by trial and error or by configuring
+    ffmped.
+    Links: 
+    Configuring ffmped: http://stackoverflow.com/questions/11444926/videocapture-is-not-working-in-opencv-2-4-2/11465097#11465097
+    A blog: www.pyimagesearch.com/2016/02/22/writing-to-video-with-opencv/
+
+    The issue can be solved as temporarily using scikit-video library. Heres the link
+    Git link: https://github.com/ContinuumIO/anaconda-issues/issues/121
+"""
+def cam_writer(device=0):
+    cap = cv2.VideoCapture(0)
+    fourcc = cv2.VideoWriter_fourcc(*'H264')
+    out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
+    # Define the codec and create VideoWriter object
+
+    while(cap.isOpened()):
+      ret, frame = cap.read()
+      if ret==True:
+        #frame = cv2.flip(frame,0)
+        out.write(frame)
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+          break
+      else:
+        print("Problem with reading video frames")
+        break
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
+
+def vid_view():
+    #help(skvideo.io.VideoCapture)
+    cap = skvideo.io.VideoCapture(str("./test.mp4"))  # u = unicode(s, "utf-8")
+    ret, frame1 = cap.read()
+    imshow('rah', frame)
+
+
+if __name__ == '__main__': 
+    #cam_writer()
+    #vid_view()
+    vid_to_hdf5()
